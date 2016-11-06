d688b00699350b747d60d6ffe8eac754e0203561
John T. White <john.t.white@gmail.com>
2016-11-06 12:38:23 -0500

    Added initial projects.

---
+++ src/StaticDotNet.ParameterValidation/ParameterValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticDotNet.ParameterValidation
+{
+    public class ParameterValidator<TParameter>
+    {
+		public ParameterValidator( string parameterName, TParameter parameterValue )
+		{
+			this.ParameterName = parameterName;
+			this.ParameterValue = parameterValue;
+		}
+
+		public string ParameterName { get; }
+
+		public TParameter ParameterValue { get; }
+    }
+}
---
+++ tests/StaticDotNet.ParameterValidation.UnitTests/ParameterValidator_ctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StaticDotNet.ParameterValidation.UnitTests
+{
+    public class ParameterValidator_ctor
+    {
+		[Fact]
+		public void ParameterValidator_ctor_InitializesCorrectly()
+		{
+			string parameterName = "ParameterName";
+			object parameterValue = new object();
+
+			ParameterValidator<object> parameterValidator = new ParameterValidator<object>( parameterName, parameterValue );
+
+			Assert.Equal( parameterName, parameterValidator.ParameterName );
+			Assert.Same( parameterValue, parameterValidator.ParameterValue );
+		}
+    }
+}
