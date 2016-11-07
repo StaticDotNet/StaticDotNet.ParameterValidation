360b5b4ca20885aede786c2fc62aa059f9d325d1
John T. White <john.t.white@gmail.com>
2016-11-07 14:03:43 -0500

    Added Parameter class.

---
+++ src/StaticDotNet.ParameterValidation/Parameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticDotNet.ParameterValidation
+{
+    public static class Parameter
+    {
+		public static ParameterValidator<TParameter> Validate<TParameter>( TParameter value )
+		{
+			return Parameter.Validate( value, null );
+		}
+
+		public static ParameterValidator<TParameter> Validate<TParameter>( TParameter value, string name )
+		{
+			return new ParameterValidator<TParameter>( name, value );
+		}
+    }
+}
---
+++ tests/StaticDotNet.ParameterValidation.UnitTests/Parameter_Validate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StaticDotNet.ParameterValidation.UnitTests
+{
+    public class Parameter_Validate
+    {
+		[Fact]
+		public void Parameter_Validate_WithValueReturnsCorrectly()
+		{
+			object value = new object();
+
+			ParameterValidator<object> result = Parameter.Validate( value );
+
+			Assert.Same( value, result.Value );
+			Assert.Null( result.Name );
+		}
+
+		[Fact]
+		public void Parameter_Validate_WithValueAndNameReturnsCorrectly()
+		{
+			object value = new object();
+			string name = "Name";
+
+			ParameterValidator<object> result = Parameter.Validate( value, name );
+
+			Assert.Same( value, result.Value );
+			Assert.Equal( name, result.Name );
+		}
+	}
+}
